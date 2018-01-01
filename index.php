251575e8ba59e050c4a977c165afee3f8b41787c
DESKTOP-6GE6FHU\kamru <kamrulhasanshuvo04@gmail.com>
2018-01-01 22:38:02 +0600

    basic intro

---
+++ index.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html>
+    
+    <head>
+    <title>Learn with me - array sourch code for practise</title>    
+    </head>
+    <body style="margin:0 auto;width:960px;background:#EAEAEA">
+        
+        <div style="background-color:<?php
+echo "white";
+?>;width:100%;margin:0 auto;overflow: hidden;" >
+        <div style="background-color:<?php
+echo "black";
+?>;overflow: hidden;" >
+            
+            <h2 style="color:white;text-align:center;font-size:48px;padding: 27px;">Php tutorial with shuvo</h2>
+            <h2 style="color: red;text-align: center;font-size: 28px;padding: 0px 3px 18px 0px;line-height: 0px;"><a href="http://kamrulhasanshuvo.info">with shuvo</a> </h2>
+        </div>
+        <div id="content" style="min-height:68vh;padding:20px">
+        <h2 style="font-size:38px;text-align:center">OOP tutorials</h2>
+      
+        
+               <br>
+               <h2>Class - Method - Object</h2>
+               <hr>
+               <br> 
+         <?php
+            class shuvo{
+                
+            public $age;
+            public $partner;
+            public function sMethod(){
+                 echo $this->age;
+               
+                
+                
+            }    
+            }
+            
+             $sObject = new shuvo; 
+             $sObject->age=26;
+             $sObject->sMethod();
+             
+            
+            ?>
+       
+               
+        </div>
+       <div style="background-color:<?php
+echo "black";
+?>;width:100%;margin:0 auto;overflow: hidden;" id="header">
+            
+       
+            <p style="color: red;text-align: center;font-size: 28px;padding: 0px 3px 18px 0px;line-height: 0px;"><a href="http://kamrulhasanshuvo.info">with shuvo</a> </p>
+        </div>
+        </div>
+        
+    </body>
+</html>
